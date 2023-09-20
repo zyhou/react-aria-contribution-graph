307860896db1e4ae019e1a58a2125eeab9816076
zyhou <richard.maxime9@gmail.com>
2023-09-20 23:33:45 +0200

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="flex flex-col items-center max-w-xl">
+      <h1 className="text-3xl font-bold mt-8 mb-16 text-gray-600">
+        React Aria Contribution Calandar
+      </h1>
+      <h2 className="ml-12 font-bold text-3xl text-gray-700">Calendar</h2>
+    </div>
+  );
 }
-
-export default App
